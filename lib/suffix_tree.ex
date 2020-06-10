a2666d301f4f69c1212a2522862e77271eb9e2d5
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2020-06-10 17:45:54 +1000

    initial commit

---
+++ lib/suffix_tree.ex
@@ -0,0 +1,18 @@
+defmodule SuffixTree do
+  @moduledoc """
+  Documentation for `SuffixTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SuffixTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/suffix_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule SuffixTreeTest do
+  use ExUnit.Case
+  doctest SuffixTree
+
+  test "greets the world" do
+    assert SuffixTree.hello() == :world
+  end
+end
