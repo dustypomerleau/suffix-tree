1a7475144fde034ded7685b00ee923783ed3b481
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2020-06-13 21:15:02 +1000

    add functions to add or remove children, find leaves or root

--- lib/suffix_tree/node.ex
+++ lib/suffix_tree/node.ex
@@ -7,8 +7,33 @@ defmodule SuffixTree.Node do
     :children,
     :link
   ]
-  defstruct label: "",
+  defstruct label: nil,
             parent: nil,
             children: [],
             link: nil
+
+  def root?(%{parent: parent}) do
+    !parent
+  end
+
+  def leaf?(%{children: children}) do
+    Enum.empty?(children)
+  end
+
+  def add_child(%{children: children} = parent, child) do
+    child = %{child | parent: parent}
+    parent = %{children: [child | children]}
+    {:ok, parent}
+  end
+
+  def remove_child(%{children: children} = parent, label) do
+    child = get_child(children, label)
+    children = List.delete(children, child)
+    parent = %{parent | children: children}
+    {:ok, parent}
+  end
+
+  def get_child(children, label) do
+    Enum.find(children, fn child -> child.label == label end)
+  end
 end
