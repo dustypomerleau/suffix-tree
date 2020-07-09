b3a949ff70c12c8769664df9a767a474b3e74c54
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2020-07-09 23:16:09 +1000

    switch to `id` for holding references to parent and children

--- lib/suffix_tree/node.ex
+++ lib/suffix_tree/node.ex
@@ -3,8 +3,6 @@
 # structs
 # ukkonen2
 
-# import, alias, use
-
 defmodule SuffixTree.Node do
   @moduledoc false
 
@@ -22,11 +20,16 @@ defmodule SuffixTree.Node do
 
   @type t :: %Node{
           id: String.t(),
-          parent: Node.t(),
+          # parent.id
+          parent: String.t(),
+          # {hash, range}
           label: {String.t(), Range.t()},
+          # {hash, start_index}
           leaves: [{String.t(), integer()}],
-          children: [Node.t()],
-          link: Node.t()
+          # [child.id]
+          children: [String.t()],
+          # node.id
+          link: String.t()
         }
 
   def new_node() do
@@ -49,31 +52,37 @@ defmodule SuffixTree.Node do
     Enum.empty?(children)
   end
 
-  def add_child(%{children: children} = parent, child) do
-    {:ok, child} = add_parent(parent, child)
-    children = [child | children] |> Enum.sort(Node)
+  def add_child(
+        %{id: parent_id, children: children} = parent,
+        %{id: child_id} = child
+      ) do
+    {:ok, child} = add_parent(parent_id, child)
+    children = [child_id | children] |> Enum.sort()
+    parent = %{parent | children: children}
+    {:ok, parent, child}
+  end
+
+  def add_parent(parent_id, child) do
+    {:ok, %{child | parent: parent_id}}
+  end
+
+  def remove_child(%{children: children} = parent, child_id) do
+    children = List.delete(children, child_id)
     {:ok, %{parent | children: children}}
   end
 
-  def add_parent(parent, child) do
-    {:ok, %{child | parent: parent}}
-  end
+  # # perhaps rewrite this if needed
+  # def get_child(children, hash) do
+  #   Enum.find(children, fn child -> child == hash end)
+  # end
 
   # `compare/2 is used by `Enum.sort(list, Node)`
+  # since you are sorting ids rather than nodes this function may not be needed
   def compare(%{label: label1} = _node1, %{label: label2} = _node2) do
     case {label1, label2} do
       {label1, label2} when label1 > label2 -> :gt
       {label1, label2} when label1 < label2 -> :lt
       _ -> :eq
     end
-  end
-
-  def remove_child(%{children: children} = parent, child) do
-    children = List.delete(children, child)
-    {:ok, %{parent | children: children}}
-  end
-
-  def get_child(children, hash) do
-    Enum.find(children, fn child -> child == hash end)
   end
 end
